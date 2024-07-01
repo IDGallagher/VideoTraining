10eae0144b6d6d31a59bf3a4aacb0ec2ab858960
idgallagher <idgallagher@gmail.com>
2024-07-01 17:34:39 +0000

    runpod changes

--- download-webvid-test.sh
+++ download-webvid-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export TF_ENABLE_ONEDNN_OPTS=0
-video2dataset --tmp_dir=".\tmp" --url_list=".\datasets\test.csv" --input_format="csv" --output-format="webdataset" --output_folder=".\data\webvid10m-test" --url_col="contentUrl" --caption_col="name" --save_additional_columns="[videoid]" --enable_wandb=True --config=default
+./video2dataset/.venv/Scripts/video2dataset.exe --tmp_dir=".\tmp" --url_list="./datasets/test.csv" --input_format="csv" --output-format="webdataset" --output_folder="./data/webvid10m-test" --url_col="contentUrl" --caption_col="name" --save_additional_columns="[videoid]" --enable_wandb=True --config=".\configs\default.yaml"
